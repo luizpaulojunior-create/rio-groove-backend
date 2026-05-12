c8c1ca954232bbbc67315034dec78081b76376fd
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-12 20:34:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+app.get('/api/config/public', (req, res) => {
+  res.json({
+    checkoutEndpoint: 'https://rio-groove-backend.onrender.com/api/checkout'
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+});
 });
