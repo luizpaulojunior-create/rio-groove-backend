2514b937838a157d3581e50e951bd6292dc57163
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-12 19:59:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-require('./src/server');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Rio Groove Backend Online 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
