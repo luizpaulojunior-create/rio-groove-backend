dc1b41b1250932cbbfb2cadc8a5bb5ae839386d1
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-15 13:12:12 -0300

    Delete rio-groove-backend directory

--- rio-groove-backend/src/routes/config.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getPublicConfig } = require('../controllers/config.controller');
-
-const router = express.Router();
-
-router.get('/api/config/public', getPublicConfig);
-
-module.exports = router;
