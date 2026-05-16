83a49cd6d308b8343089ea64a8e0678878129979
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-16 00:23:42 -0300

    Delete rio-groove-backend directory

--- rio-groove-backend/src/routes/webhooks.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { mercadoPagoWebhook } = require('../controllers/webhook.controller');
-
-const router = express.Router();
-
-router.post('/api/webhooks/mercadopago', mercadoPagoWebhook);
-
-module.exports = router;
