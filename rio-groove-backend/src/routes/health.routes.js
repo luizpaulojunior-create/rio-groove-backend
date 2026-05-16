f1d2d9b4e3d857b4b924e1cde67181fa92befcca
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-15 21:11:11 -0300

    Delete rio-groove-backend directory

--- rio-groove-backend/src/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { healthCheck } = require('../controllers/health.controller');
-
-const router = express.Router();
-
-router.get('/health', healthCheck);
-router.get('/api/health', healthCheck);
-
-module.exports = router;
