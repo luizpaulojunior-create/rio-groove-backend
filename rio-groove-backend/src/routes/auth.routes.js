48eb931d456f454d2557d0697aee0340ccd25285
luizpaulojunior-create <luizpaulojunior@gmail.com>
2026-05-16 01:04:11 -0300

    Delete rio-groove-backend directory

--- rio-groove-backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginMelhorEnvio, callbackMelhorEnvio } = require('../controllers/auth.controller');
-
-const router = express.Router();
-
-router.get('/melhor-envio/login', loginMelhorEnvio);
-router.get('/melhor-envio/callback', callbackMelhorEnvio);
-
-module.exports = router;
